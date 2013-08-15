de3e7b2fb508476af0f5e1cd942e1c28c96d9fdb
wenyx <345612098@qq.com>
2013-08-15 07:23:47 -0700

    测试springmvc

---
+++ code/src/main/java/com/cqa/sys/service/SysUserService.java
@@ -0,0 +1,13 @@
+package com.cqa.sys.service;
+
+import com.cqa.service.impl.BaseServiceImpl;
+
+public class SysUserService extends BaseServiceImpl {
+
+	public static final String BEAN_ID = "sysUserService";
+	
+	public void loadSysuser(){
+		
+		logger.debug("test service ...");
+	}
+}
